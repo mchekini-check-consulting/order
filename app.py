d361ff582c197a04f280fe27bbc9e0f4ee21af95
GOUMEZIANE Hocine <h_goumeziane@hotmail.fr>
2023-12-03 21:28:40 +0100

    feat: add port env variable

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
+FLASK_RUN_PORT = os.getenv('FLASK_RUN_PORT', '8080')
+
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Hello World from Order!'
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=FLASK_RUN_PORT, debug=True)
